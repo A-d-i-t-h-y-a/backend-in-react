4a20e1f756cfb33afc43c2014c6958221fd74efd
Adithya <periketi.adithyachary@gmail.com>
2023-01-23 18:29:06 +0530

    connected to mongodb atlas

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
-const mongoURI = "mongodb://localhost:27017/inotebook"
+// const mongoURI = "mongodb://localhost:27017/inotebook"
+const mongoURI = "mongodb+srv://adithya:first_cluster@cluster0.oilwegb.mongodb.net/inotebook"
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI, ()=>{
